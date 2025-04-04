ae0216320b05ac14ffed5ce77c91ef39e6d11bb4
Tornrosen <elec11anna@hotmail.com>
2025-04-04 15:11:08 +0200

    ordna med formulär

--- server.js
+++ server.js
@@ -4,6 +4,17 @@
 const express = require("express")
 const app = express()
 const port = 3000
+const bodyParser = require("body-parser");
+
+const courseListEl =[{
+  courseName: "Backend-utveckling",
+  courseCode: "Dt207G",
+  progression: "B",
+  syllabus: "url",
+}];
+
+//body-parser
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //View engine: ejs
 app.set ("View engine", "ejs");
@@ -13,19 +24,66 @@ app.use(express.static("Public"));
 
 //route
 app.get("/", (req, res) => {
-  const courseList =[];
-  res.render("index.ejs", {courseList});
+  res.render("index.ejs", {courseListEl});
 })
 
 app.get("/addcourse", (req, res) =>{
-  res.render("addcourse.ejs");
+  res.render("addcourse.ejs", {
+    errors:[],
+    newCourseCode: "",
+    newCourseName: "",
+    newProgression: "",
+    newSyllabus: ""
+  });
 })
 
 app.get("/about", (req, res) => {
   res.render("about.ejs");
 })
 
-//start
+//posta formulärdata
+app.post("/addcourse", (req, res) => {
+  let newCourseName = req.body.courseName;
+  let newCourseCode = req.body.courseCode;
+  let newProgression = req.body.progression;
+  let newSyllabus = req.body.syllabus;
+  let errors = [];
+
+  if(newCourseName==="") {
+   errors.push("Ange kursnamn");
+  }
+  if(newCourseCode==="") {
+    errors.push ("Ange en kurskod");
+  }
+  if(newProgression==="") {
+    errors.push ("Ange progression");
+  }
+  if(newSyllabus==="") {
+    errors.push ("Ange länk till kursplan");
+  }
+  
+  if (errors.length===0) {
+  courseListEl.push({
+    courseName: newCourseName,
+    courseCode: newCourseCode,
+    progression: newProgression,
+    syllabus: newSyllabus
+  })
+newCourseName="";
+newCourseCode="";
+newProgression="";
+newSyllabus="";
+}
+  res.render("addcourse.ejs", {
+    errors:errors,
+    newCourseCode: newCourseCode,
+    newCourseName: newCourseName,
+    newProgression: newProgression,
+    newSyllabus: newSyllabus
+  });
+});
+
+//starta
 app.listen(port, () =>{
   console.log("Example app listening on port: " +port);
 })
